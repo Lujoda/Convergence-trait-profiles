94bea28a42e27483cb78e7cf700b28c3d1925045
Lujoda <luicdamian@gmail.com>
2020-11-25 12:57:41 +0100

    Classification subsets

--- Comparison_between_climatic_zones/R/01_data_prep_ecoreg_traits.R
+++ Comparison_between_climatic_zones/R/01_data_prep_ecoreg_traits.R
@@ -76,14 +76,14 @@ traits_EU_lf_sb <- base::merge(x = traits_EU_lf_sb,
 test <- traits_EU_lf_sb[!is.na(traits_EU_lf_sb$ecoregion), ]
 reshape2::dcast(test, ID_AQEM + KoppenClas + SecKC ~ key_col)
 
-
+#Necessary packages tidyverse, foreign
 
 # Loading data
 traits_full <- read.csv("traits_koppen_wide.csv")
 
 
 # Subset 1:  ID_AQEM  KoppenClas  SecKC  Ecoregions
-eco_subs <- select(traits_full, ID_AQEM, grep("ER", names(traits_full)), ecoregion, KoppenClas, SecKC, key_col)
+eco_subs <- select(traits_full, ID_AQEM, ecoregion, KoppenClas, SecKC)
 
 # Subset 2: Taxonom. Info + Merkmale + Ecoregions + ID_AQEM
 traits_sub <- select(traits_full, -grep("ER", names(traits_full)), -key_col, -KoppenClas,-SecKC, -grep("prim",names(traits_full)))
@@ -97,3 +97,23 @@ conf <- read.dbf("ecoregions/Ecoregions.dbf", as.is=FALSE)
 # Adding confidence data to Subset 1
 eco_sub <- base::merge(eco_subs, conf[,c(5,11)], by.x="ecoregion", by.y="NAME")
 
+# Correcting error in Koppen Geiger Classification naming 
+eco_sub$KoppenClas[eco_sub$KoppenClas=="Bsk"] <- "BSk"
+
+# Adding classification descriptions to ecoregions
+eco_sub2 <- mutate(eco_sub, Classification_Description1 = case_when(eco_sub$KoppenClas == "Dfb" ~ "Cold_nodryseason_warmsummer",
+                                                                    eco_sub$KoppenClas == "ET"~"Polar_tundra",
+                                                                    eco_sub$KoppenClas == "Dfc"~"Cold_nodryseason_coldsummer",
+                                                                    eco_sub$KoppenClas == "Cfb"~"Temperate_nodryseason_warmsummer",
+                                                                    eco_sub$KoppenClas == "BSk"~"Arid_steppe_cold",
+                                                                    eco_sub$KoppenClas == "Csa"~"Temperate_drysummer_hotsummer"))
+
+
+
+# Subsetting acording to climate classification
+eco_Dfb <- subset(eco_sub2, eco_sub2$KoppenClas == "Dfb")
+eco_ET <- subset(eco_sub2, eco_sub2$KoppenClas == "ET")
+eco_Dfc <- subset(eco_sub2, eco_sub2$KoppenClas == "Dfc")
+eco_Cfb <- subset(eco_sub2, eco_sub2$KoppenClas == "Cfb")
+eco_BSk <- subset(eco_sub2, eco_sub2$KoppenClas == "BSk")
+eco_Csa <- subset(eco_sub2, eco_sub2$KoppenClas == "Csa")
